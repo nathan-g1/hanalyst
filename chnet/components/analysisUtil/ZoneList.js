e4fdc1f4098a7776ca278308a7714cd03616a849
Nathan Getachew <nathygech111@gmail.com>
2020-02-16 22:33:17 +0300

    feat: add zone list with zone number

---
+++ chnet/components/analysisUtil/ZoneList.js
@@ -0,0 +1,17 @@
+// Zone List to diplay on the field
+const ZoneList = [
+
+    { id: 0, zoneNumber: 3 },
+    { id: 1, zoneNumber: 1 },
+    { id: 2, zoneNumber: 2 },
+    { id: 3, zoneNumber: 6 },
+    { id: 4, zoneNumber: 5 },
+    { id: 5, zoneNumber: 4 },
+    { id: 6, zoneNumber: 9 },
+    { id: 7, zoneNumber: 8 },
+    { id: 8, zoneNumber: 7 },
+    { id: 9, zoneNumber: 12 },
+    { id: 10, zoneNumber: 11 },
+    { id: 11, zoneNumber: 10 },
+];
+export default ZoneList;
