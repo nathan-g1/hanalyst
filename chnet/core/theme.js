c68a292b3032f88f68de592b89e3a65713f74316
Nathan Getachew <nathygech111@gmail.com>
2020-02-25 22:26:26 +0300

    fix: theme depends on native-paper color

--- chnet/core/theme.js
+++ chnet/core/theme.js
@@ -1,7 +1,11 @@
+import DefaultTheme from 'react-native-paper';
+
 export const theme = {
-    colors: {
-      primary: '#600EE6',
-      secondary: '#414757',
-      error: '#f13a59',
-    },
-  };
+  ...DefaultTheme,
+  colors: {
+    // ...DefaultTheme.colors,
+    primary: '#600EE6',
+    secondary: '#414757',
+    error: '#f13a59',
+  },
+};
