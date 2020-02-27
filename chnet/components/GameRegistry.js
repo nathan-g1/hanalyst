23f218e7d30e18b82db2703c0219f48dc5007a9e
Nathan Getachew <nathygech111@gmail.com>
2020-02-27 17:52:29 +0300

    feat: create a Game Regsitry component
    - to be filled before every game by the analyst

---
+++ chnet/components/GameRegistry.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
+export default class GameRegistry extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Game Information</Text>
+            </View>
+        );
+    }
+}
